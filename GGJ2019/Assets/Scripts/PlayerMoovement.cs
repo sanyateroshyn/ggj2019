ddac5c76e890ec37ff3f1b198ce93438d6735d9e
GGJGameOff <ibiza347@gmail.com>
2019-01-25 23:49:50 +0200

    global

---
+++ GGJ2019/Assets/Scripts/PlayerMoovement.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMoovement : MonoBehaviour {
+
+	public CharacterController2D controller;
+
+	public float runSpeed = 40f;
+
+	float horizontalMove = 0f;
+	bool jump = false;
+	bool crouch = false;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+		horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+
+		if (Input.GetButtonDown("Jump")) {
+
+			jump = true;
+		}
+		if(Input.GetButtonDown("Crouch")) {
+			crouch = true;
+		}
+		else if(Input.GetButtonUp("Crouch")) {
+			crouch = false;
+		}
+
+
+
+	}
+
+
+	private void FixedUpdate() {
+
+
+		controller.Move(horizontalMove * Time.fixedDeltaTime , crouch, jump);
+
+		jump = false;
+
+	}
+}
