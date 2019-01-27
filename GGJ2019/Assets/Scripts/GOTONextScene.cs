7aad5097b15532d30c23cfb26fb4a28edb91b526
Teroshyn.Alexandr <sanya.teroshyn@gmail.com>
2019-01-27 04:09:54 +0200

    - new enemy

---
+++ GGJ2019/Assets/Scripts/GOTONextScene.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GOTONextScene : MonoBehaviour {
+
+	public int SceneIndex;
+
+	public void OnEndAnim() 
+		{
+		SceneManager.LoadScene(SceneIndex);
+	}
+
+	public void OnClickStartGameButton(int SceneIndex) 
+		{
+		SceneManager.LoadScene(SceneIndex);
+	}
+}
